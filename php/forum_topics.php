792939c4f70b78a746fbbc1d3e731a57b687e413
Loic Aulard <aulard.loic@gmail.com>
2019-03-05 08:18:56 +0100

    Mise a jour des fichier forums

---
+++ php/forum_topics.php
@@ -0,0 +1,154 @@
+
+
+
+
+<!-- A ne pas utiliser !!-->
+
+<!-- Suppression future-->
+
+
+
+
+
+
+<!DOCTYPE html>
+
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<link rel="stylesheet" type="text/css" href="../css/design.css">
+	<link rel="stylesheet" type="text/css" href="../css/Forum.css">
+</head>
+<body>
+
+<!-- Barre de navigation -->
+<?php require_once ('navigation.html') ?>
+
+  <div class="listeTopics" id = "forumGeneral">
+    <div class="ligne bandeau">
+        <div class= "tableHeader">
+               Forum Général   
+        </div>
+        <div class= "tableHeader">
+                Auteur
+        </div>
+        <div class= "tableHeader">
+                Date Dernière Modif.
+        </div>
+    </div>
+    <div class="ligne">
+        <div class="nomTopic">
+            <a href="#a" >Horizon Japon</a>  
+        </div>
+        <div class="Auteur">
+            Guillaume Hurvois
+        </div>
+        <div class="DateModif">
+            7-12-2018
+        </div>
+    </div>
+
+    <div class="ligne">
+        <div class="nomTopic">
+            <a href="#a" >Le ski coûte trop cher!</a>  
+        </div>
+        <div class="Auteur">
+            Jean Neymar
+        </div>
+        <div class="DateModif">
+            5-12-2018
+        </div>
+    </div>
+
+    <div class="ligne">
+        <div class="nomTopic">
+            <a href="#a" >J'ai perdu mon sac</a>  
+        </div>
+        <div class="Auteur">
+           Wahbi Ayoub
+        </div>
+        <div class="DateModif">
+           19-11-2018
+        </div>
+    </div>
+
+    <div class="ligne">
+        <div class="nomTopic">
+            <a href="#a" >Winter is coming</a>  
+        </div>
+        <div class="Auteur">
+            Jon Snow
+        </div>
+        <div class="DateModif">
+            17-11-2018
+        </div>
+    </div>
+    
+
+
+
+
+
+
+
+  </div>
+  <div class = "listeTopics" id = "forumParticulier">
+        <div class="ligne bandeau">
+            <div class= "tableHeader">
+                Forum Informatique    
+            </div>
+            <div class= "tableHeader">
+                Auteur
+            </div>
+            <div class= "tableHeader">
+                Date Dernière Modif.
+            </div>
+        </div>
+        <div class="ligne">
+            <div class="nomTopic">
+                <a href="#a" >Cours de big data</a>  
+            </div>
+            <div class="Auteur">
+                Loïc Aulard
+            </div>
+            <div class="DateModif">
+                1-12-2018
+            </div>
+        </div>
+
+        <div class="ligne">
+            <div class="nomTopic">
+                <a href="#a" >Info2: cours annulé</a>  
+            </div>
+            <div class="Auteur">
+                Thibaud carré
+            </div>
+            <div class="DateModif">
+                4-12-2018
+            </div>
+        </div>
+
+        <div class="ligne">
+            <div class="nomTopic">
+                <a href="#a" >Logique: Partiel de l'année dernière?</a>  
+            </div>
+            <div class="Auteur">
+                Ilan Bitton
+            </div>
+            <div class="DateModif">
+                1-12-2018
+            </div>
+        </div>
+
+  </div>
+
+
+
+
+
+  <footer>
+
+  </footer>
+</body>
+
+</html>
