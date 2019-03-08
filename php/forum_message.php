9a2e577cbf5a3291a94cef9655f8c3a6cd12430b
Loic Aulard <aulard.loic@gmail.com>
2019-03-08 13:34:49 +0100

    Mesage forum MaJ

--- php/forum_message.php
+++ php/forum_message.php
@@ -3,15 +3,14 @@
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<link rel="stylesheet" type="text/css" href="../css/design.css">
-	<link rel="stylesheet" type="text/css" href="../css/Forum.css">
+	<link rel="stylesheet" type="text/css" href="../css/main.css">
+	<link rel="stylesheet" type="text/css" href="../css/forum.css">
 </head>
 <body>
-<<<<<<< HEAD:php/forum_message.html
   
-
+<?php require_once ('navigation.html') ?>
   <div class="container top-page">
-        <div class="row block">
+        <div class="row">
             <div class="col">
                 <!--Messages are here !-->
                 <!--A message-->
@@ -32,100 +31,30 @@
                             Bla Bla Bla
                     </div>
                 </div>
-=======
+                <div class="row message">
+                    <!--Information about the message-->
+                    <div  class="col-md-2 user-info ">
+                        <div class="row user-name">
+                            <div class="col"><p>NAME</p></div>
+                        </div>
+                        <div class="row message-date">
+                            <div class="col-md">Posté le : </div>
+                            <div class="col-md">07/02/2019 à 17h30</div>
+                        </div>
+                    </div>
 
-<!-- Barre de navigation -->
-	<nav>
-		<ul>
-			<li><a href="#a"><img src="../img/accueil.png"><div class="titre">Accueil</div></a></li>
-			<li><a href="#a"><img src="../img/equipe.png" style="padding-top:2px">   <div class="titre">L'équipe</div></a></li>
-			<li><a href="#a">Clubs</a></li>
-			<li><a href="#a">Evénements</a></li>
-			<li><a href="#a">Boutique</a></li>
-			<li><a href="Forum.html">Forum</a></li>
-			<li id="li_contact"><a href="#a">Contact</a></li>
-			<li id="li_facebook"><a href="#a">F</a></li>
-			<li id="li_connexion" onclick="connexion()"><a href="#a">AA</a></li>
-		</ul>
-	</nav>
-
-	<div id="connexion"></div>
-
-<div style="top:0;z-index: -100;position: sticky;">
-<video width="100%"autoplay muted loop style="margin-top: -3%">
-    <source src="../img/composition_planete_tournante_compressé.mp4" type="video/mp4">
-</video> 
-</div>
-
-  <div class="listeTopics" id = "forumGeneral">
-    <div class="ligne bandeau">
-        <div class= "tableHeader">
-               Forum Général   
+                    <!--Content of the message-->
+                    <div class="col-md-9 message-content">
+                            Bla Bla Bla
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class= "tableHeader">
-                Auteur
-        </div>
-        <div class= "tableHeader">
-                Date Dernière Modif.
-        </div>
-    </div>
-    <div class="ligne">
-        <div class="nomTopic">
-            <a href="#a" >Horizon Japon</a>  
-        </div>
-        <div class="Auteur">
-            Guillaume Hurvois
-        </div>
-        <div class="DateModif">
-            7-12-2018
-        </div>
-    </div>
-
-    <div class="ligne">
-        <div class="nomTopic">
-            <a href="#a" >Le ski coûte trop cher!</a>  
-        </div>
-        <div class="Auteur">
-            Jean Neymar
-        </div>
-        <div class="DateModif">
-            5-12-2018
-        </div>
-    </div>
-
-    <div class="ligne">
-        <div class="nomTopic">
-            <a href="#a" >J'ai perdu mon sac</a>  
-        </div>
-        <div class="Auteur">
-           Wahbi Ayoub
-        </div>
-        <div class="DateModif">
-           19-11-2018
-        </div>
-    </div>
-
-    <div class="ligne">
-        <div class="nomTopic">
-            <a href="#a" >Winter is coming</a>  
-        </div>
-        <div class="Auteur">
-            Jon Snow
-        </div>
-        <div class="DateModif">
-            17-11-2018
-        </div>
-    </div>
->>>>>>> 9883a74aa5e591654da932240d9f726bb79a9488:php/forum.html
-    
-
-
-
-
-
-
-
   </div>
+
+
+
+  <!--
   <div class = "listeTopics" id = "forumParticulier">
         <div class="ligne bandeau">
             <div class= "tableHeader">
@@ -175,13 +104,13 @@
         </div>
 
   </div>
-
+-->
 
 
 
 
   <footer>
-
+    <?php require_once ('footer.html') ?>
   </footer>
 </body>
 
