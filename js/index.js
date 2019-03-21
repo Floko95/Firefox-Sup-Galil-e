9193d76c0927f18d3732806ba3c9a005662e7343
yjacoud <44291722+yjacoud@users.noreply.github.com>
2019-03-21 20:02:46 +0100

    Delete index.js

--- js/index.js
+++
@@ -1,8 +0,0 @@
-function changeState(form) {
-	if(form.regagree.checked == true) {form.inscription.disabled = false }
-	if(form.regagree.checked == false) {form.inscription.disabled = true }
-}
-
-if (document.getElementById("check") != null){
-	document.getElementById("check").addEventListener("click", function(){changeState(this.form);});
-}
