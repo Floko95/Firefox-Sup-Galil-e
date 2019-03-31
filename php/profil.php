07057f137bcc007ca3dccd9badfcdd9370285679
yjacoud <44291722+yjacoud@users.noreply.github.com>
2019-03-31 19:46:20 +0200

    Add files via upload

--- php/profil.php
+++ php/profil.php
@@ -25,15 +25,82 @@ require_once 'inc/fonctions.php';
 ?>
 
 <?php
-# Changement de formation d'un CP2I
-?>
+# Changement de formation et/ou de promotion
+if (isset($_POST['modifierFormation']) && $_POST['modifierFormation'] == 'Valider') {
 
-<?php
-# Changement de promotion
+	$errors = array();
+	
+	# Si on a coché le changement de formation mais qu'on n'est pas CP2I
+	if (isset($_POST['cp2iterminee']) && $_SESSION['formation'] != 'CP2I') {
+		$errors['changementFormation'] = "Seuls les CP2I peuvent changer de formation";
+	}
+	
+	# Si on a coché le changement de formation mais qu'on n'a pas choisi de nouvelle formation
+	if (isset($_POST['cp2iterminee']) && empty($_POST['formation'])) {
+		$errors['nouvelleFormation'] = "Vous devez choisir une nouvelle formation";
+	}
+	
+	# Si la nouvelle promotion est manquante
+	if (empty($_POST['promotion'])) {
+		$errors['nouvellePromotion'] = "Vous devez choisir une nouvelle promotion";
+	}
+	
+	# S'il n'y a pas d'erreurs, on modifie les informations
+	if (empty($errors)) {
+		# On met à jour la formation
+		if (isset($_POST['cp2iterminee'])) {
+			$req = $bdd->prepare('SELECT COUNT(*) FROM attributionRolesAuxEtudiants WHERE id = ? AND idRoles = 4');
+			$req->execute(array($_SESSION['id']));
+			$data = $req->fetch();
+			if ($data[0] == 0) {
+				$req = $bdd->prepare('INSERT INTO attributionRolesAuxEtudiants(id, idRoles) VALUES(:id, :idRoles)');
+				$req->execute(array(
+					'id' => $_SESSION['id'],
+					'idRoles' => 4));
+			}
+			$req = $bdd->prepare('UPDATE ETUDIANTS SET formation = ? WHERE id = ?');
+			$req->execute(array($_POST['formation'], $_SESSION['id']));
+			$_SESSION['formation'] = $_POST['formation'];
+		}
+		# On met à jour la promotion
+		$req = $bdd->prepare('UPDATE ETUDIANTS SET promotion = ? WHERE id = ?');
+		$req->execute(array($_POST['promotion'], $_SESSION['id']));
+		$success['modificationsEffectuees'] = "Les modifications ont bien été prises en compte";
+		$_SESSION['promotion'] = $_POST['promotion'];
+	}
+	
+}
 ?>
 
 <?php
 # Changement de mot de passe
+if (isset($_POST['modifierMdp']) && $_POST['modifierMdp'] == 'Valider') {
+
+	$errors = array();
+	
+	# Si le mot de passe actuel n'est pas bon
+	if (empty($_POST['mdpActuel'])) {
+		$errors['mdpActuel'] = "Le mot de passe actuel entré n'est pas le bon";
+	} else {
+		if (!password_verify($_POST['mdpActuel'], $etudiant['mdp'])) {
+			$errors['mdpActuel'] = "Le mot de passe actuel entré n'est pas le bon";
+		}
+	}
+
+	# Si le nouveau mot de passe n'est pas conforme ou la confirmation est différente
+	if (empty($_POST['mdp']) || strlen($_POST['mdp']) < 6) {
+		$errors['mdp'] = "Le mot de passe doit contenir au minimum 6 caractères";
+	} elseif ($_POST['mdp'] != $_POST['confirmation']) {
+		$errors['mdp'] = "Les deux mots de passe sont différents";
+	}
+	
+	# S'il n'y a pas d'erreur, on modifie le mot de passe
+	if (empty($errors)) {
+		$req = $bdd->prepare('UPDATE ETUDIANTS SET mdp = ? WHERE id = ?');
+		$req->execute(array(password_hash($_POST['mdp'], PASSWORD_BCRYPT), $_SESSION['id']));
+		$success['mdpModifie'] = "Le mot de passe a bien été modifié";
+	}
+}
 ?>
 
 <!DOCTYPE html>
@@ -48,98 +115,108 @@ require_once 'inc/fonctions.php';
 		<?php require_once ('navigation.php') ?>
 		
 		<div id="formulaire-responsive" class="clearfix inscription-form">
-			<form action="profil.php" method="post">
-				<h3>Mon compte</h3>
+			
+			<h3>Mon compte</h3>
 				
-				<!-- Affichage des erreurs -->
-				<?php if(!empty($errors)): ?>
-					<div class="alert alert-danger">
-						<strong>Vous n'avez pas rempli le formulaire correctement.</strong>
-						<ul>
-							<?php foreach ($errors as $error): ?>
-								<li><?= $error; ?></li>
-							<?php endforeach; ?>
-						</ul>
-					</div>
-				<?php endif; ?>
+			<!-- Affichage des erreurs -->
+			<?php if(!empty($errors)): ?>
+				<div class="alert alert-danger">
+					<strong>Vous n'avez pas rempli le formulaire correctement.</strong>
+					<ul>
+						<?php foreach ($errors as $error): ?>
+							<li><?= $error; ?></li>
+						<?php endforeach; ?>
+					</ul>
+				</div>
+			<?php endif; ?>
+			
+			<!-- Affichage des succès -->
+			<?php if(!empty($success)): ?>
+				<div class="alert alert-success">
+					<strong>
+					<ul>
+						<?php foreach ($success as $succes): ?>
+							<li><?= $succes; ?></li>
+						<?php endforeach; ?>
+					</ul>
+					</strong>
+				</div>
+			<?php endif; ?>
 				
-				<!-- Formulaire d'inscription -->
-				<div class="rang-form">
-					<div class="demi-colonne">
+			<div class="rang-form">
+				<fieldset>
+					<legend>Mes informations personnelles</legend>
+					<div class="colonne">
 						<label for="prenom">Prénom : </label> <?php echo text($_SESSION['prenom']); ?>
 					</div>
-					<div class="demi-colonne">
+					<div class="colonne">
 						<label for="nom">Nom : </label> <?php echo text($_SESSION['nom']); ?>
 					</div>
-				</div>
-
-				<div class="rang-form">
 					<div class="colonne">
 						<label for="numero">Numéro d'étudiant : </label> <?php echo text($etudiant['numero']); ?>
 					</div>
-				</div>
-				
-				<div class="rang-form">
-					<div class="demi-colonne">
-						<label for="mailUniv">Adresse mail universitaire :</label>
-						<input type="mail" name="mailUniv" maxlength="70" placeholder="prenom.nom@edu.univ-paris13.fr" required />
-					</div>
-					<div class="demi-colonne">
-						<label for="mailPerso">Adresse mail personnelle :</label>
-						<input type="mail" name="mailPerso" maxlength="70" />
-					</div>
-				</div>
-
-				<div class="rang-form">
-					<div class="demi-colonne">
-						<label for="formation">Formation :</label>
-						<select name="formation" id="formation" size="1">
-							<option>CP2I
-							<option>ENER
-							<option>INFO
-							<option>MACS
-							<option>TELE
-							<option>INST
-						</select>
-					</div>
-					<div class="demi-colonne">
-						<label for="promotion">Promotion :</label>
-						<?php 
-						$annee = date("Y");
-						if (date("m") >= 7) {
-							$annee++;
-						}
-						?>
-						<input type="number" name="promotion" min="2000" max=<?php echo $annee+4 ?>  step="1" value=<?php echo $annee ?> />
-					</div>
-				</div>
-
-				<div class="rang-form" id="rangAncienCP2I">
 					<div class="colonne">
-						<label><i>J'ai fais une CP2I à Sup Galilée :</i></label><br>
-						<input type="radio" id="oui" name="ancienCP2I" value="oui">Oui</input><br>
-						<input type="radio" id="non" name="ancienCP2I" value="non" checked>Non</input>
+						<label for="mailUniv">Adresse mail universitaire :</label><br> <?php echo text($etudiant['mailUniv']); ?>
 					</div>
-				</div>
-				
-				<div class="rang-form">
-					<div class="demi-colonne">
-						<label for="mdp">Mot de passe :</label>
-						<input type="password" name="mdp" placeholder="******" maxlength="30" required />
+					<div class="colonne">
+						<label for="mailPerso">Adresse mail personnelle :</label><br> <?php if (!empty($etudiant['mailPerso'])) {echo text($etudiant['mailPerso']);} else { echo '-';} ?>
 					</div>
-					<div class="demi-colonne">
-						<label for="comfirmation">Confirmation du mot de passe :</label>
-						<input type="password" name="confirmation" placeholder="******" maxlength="30" required />
-					</div>
+				</fieldset>
+				<form action="profil.php" method="post">
+					<fieldset>
+						<legend>Ma formation</legend>
+						<div class="colonne">
+							<label for="formation">Formation : </label> <?php echo text($_SESSION['formation']); ?>
+						</div>
+						<?php if ($_SESSION['formation'] == 'CP2I'): ?>
+						<div class="colonne">
+							<label><i>J'ai terminé ma CP2I :</i></label>
+							<input id="cp2iterminee" type="checkbox" name="cp2iterminee" value="valeur" />
+						</div>
+						<div class="colonne" id="nouvelleFormation" style="display:none">
+							<label for="formation">Nouvelle formation :</label>
+							<select name="formation" id="formationP" size="1">
+								<option>ENER
+								<option>INFO
+								<option>MACS
+								<option>TELE
+								<option>INST
+							</select>
+						</div>
+						<?php endif; ?>
+						<div class="colonne">
+							<label for="promotion">Promotion :</label> 
+							<?php $annee = date("Y"); if (date("m") >= 7) { $annee++; } ?>
+							<input type="number" name="promotion" min="2000" max=<?php echo $annee+4 ?>  step="1" value=<?php echo text($_SESSION['promotion']) ?> />
+						</div>
+						<div class="colonne">
+							<input type="submit" name="modifierFormation" value="Valider" />
+						</div>
+					</fieldset>
+				</form>
+				<form action="profil.php" method="post">
+					<fieldset>
+						<legend>Modifier mon mot de passe</legend>
+						<div class="colonne">
+							<label for="mdp">Mot de passe actuel:</label>
+							<input type="password" name="mdpActuel" placeholder="******" maxlength="30" required />
+						</div>
+						<div class="colonne">
+							<label for="comfirmation">Nouveau mot de passe :</label>
+							<input type="password" name="mdp" placeholder="******" maxlength="30" required />
+						</div>
+						<div class="colonne">
+							<label for="comfirmation">Confirmer :</label>
+							<input type="password" name="confirmation" placeholder="******" maxlength="30" required />
+						</div>
+						<div class="colonne">
+							<input type="submit" name="modifierMdp" value="Valider" />
+						</div>
+					</fieldset>
+				</form>
 				</div>
 
-				<div class="rang-form">
-					<div class="colonne">
-						<input id="check" type="checkbox" name="regagree" value="valeur" /> <b>J'ai lu et j'accepte les <a href="">CGU</a></b><br>
-						<input type="submit" name="inscription" value="Valider" disabled />
-					</div>
-				</div>
-			</form>
+			
 		</div>
 		
 		<br>
