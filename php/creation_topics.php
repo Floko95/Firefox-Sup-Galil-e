497c103a0dbbf98502cc505d4378c36121b8201b
yjacoud <44291722+yjacoud@users.noreply.github.com>
2019-04-07 18:35:37 +0200

    Add files via upload
    Nouveau css

--- php/creation_topics.php
+++ php/creation_topics.php
@@ -1,3 +1,8 @@
+<?php
+require_once 'inc/check_ban.php';
+require_once 'inc/fonctions.php';
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
@@ -7,18 +12,15 @@
 	</head>
 	<body>
 	
-		<?php require_once 'inc/serveur.php' ;?>
-		<?php require_once 'inc/check_ban.php'; ?>
 		<?php require_once ('navigation.php'); ?>
 
 		<?php	
-		//sélections des filières à afficher pour les boutons radio
+		// Sélections des filières à afficher pour les boutons radio
 		$forums[0] = "general";
 		$reqAdmin = $bdd->prepare('SELECT COUNT(*) FROM attributionRolesAuxEtudiants NATURAL JOIN attributionDroitsAuxRoles WHERE id = ? AND idDroits = ?');
 		$reqAdmin->execute(array($_SESSION['id'], 9));
 		$data = $reqAdmin->fetch();
-		if ($data[0] >0)//si on est un admin
-		{
+		if ($data[0] >0) { // Si on a le droit de voir tous les forums
 			$forums[1] = "INFO";
 			$forums[2] = "ENER";
 			$forums[3] = "CP2I";
@@ -27,45 +29,45 @@
 			$forums[6] = "INST";
 		
 		}
-		else
-		{
+		else {
 			$forums[1] = $_SESSION['formation'];
-			$reqrole = $bdd->prepare('SELECT COUNT(*) FROM attributionRolesAuxEtudiants  WHERE id = ? AND idRoles = 4');		//4 = role "ancien cp2i"
+			$reqrole = $bdd->prepare('SELECT COUNT(*) FROM attributionRolesAuxEtudiants WHERE id = ? AND idRoles = 4');		//4 = role "ancien cp2i"
 			$reqrole->execute(array($_SESSION['id']));
 			$check_cp2i = $reqrole->fetch();
-			if($_SESSION['formation'] != 'CP2I' and ($check_cp2i[0] != 0) )
-			{
+			if($_SESSION['formation'] != 'CP2I' and ($check_cp2i[0] != 0)) { // Si l'éleve n'est pas un cp2i MAIS qu'il est un ancien cp2i, alors on affiche le forum cp2i
 				$forums[2] = "CP2I";
-			}// si l'éleve n'est pas un cp2i MAIS qu'il est un ancien cp2i, alors on affiche le forum cp2i
+			}
 		}
 		?>
 
-		<div class="row top-page">
-			<div class="offset-md-4 col-md-3 title">
-				<h1>Création d'un nouveau topic</h1>
-			</div>
-		</div>
+		<div id="formulaire-responsive" class="clearfix inscription-form">
+			<form action="inscription.php" method="post">
+				<h3>Créer un nouveau topic</h3>
 
-		<!-- Formulaire de création d'un topic -->
-		<div class="row">
-			<div class="offset-md-4 col-md-3 block">
-				<form action="Topics.php" method="post">
-					<label for="title">Titre du topic</label><br>
-					<input type="text" name="title" required/><br>
-					<label for="tags">Tags (5 max,séparés par des espaces, 15 caractères par tag)</label><br>
-					<input type="text" name="tags" required ><br>
-					<label for="categorie">Catégorie du topic</label><br>
-					
-					<?php foreach($forums as $forum):?>
-					<input type="radio" name="categorie" value="<?php echo $forum;?>"/> Fillière <?php echo $forum;?><br> 
-					<?php endforeach;?>
-					
-					<br>
-					<label for="msg">Votre message</label><br>
-					<textarea required name="ecriture"> Message</textarea><br>
-					<input type="submit" name="connexion" value="Valider" />
-				</form>
-			</div>
+				<div class="rang-form">
+					<div class="colonne">
+						<label for="title">Nom du topic :</label><br>
+						<input type="text" name="title" required/>
+					</div>
+					<div class="colonne">
+						<label for="tags">Tags (5 max, séparés par des espaces, 15 caractères par tag)</label><br>
+						<input type="text" name="tags">
+					</div>
+					<div class="colonne">
+						<label for="categorie">Catégorie du topic :</label><br>
+						<?php foreach($forums as $forum):?>
+							<input type="radio" name="categorie" value="<?php echo $forum;?>"/> Filière <?php echo $forum;?> <br>
+						<?php endforeach;?>
+					</div>
+					<div class="colonne">
+						<label for="msg">Votre message :</label><br>
+						<textarea required name="ecriture" maxlength="2000" style="max-width:95%;width:90%; height:80px"></textarea>
+					</div>
+					<div class="colonne">
+						<input type="submit" name="connexion" value="Valider" />
+					</div>
+				</div>
+			</form>
 		</div>
 
 	</body>
